7dc518890452d6c037ecbf8f9713271ac2e1ea3e
Kirsten Barrett <kirstenne@gee-mail.com>
2015-11-17 10:43:31 +0000

    float and muted errors

--- 20151111_FRP_stuff.py
+++ 20151111_FRP_stuff.py
@@ -16,10 +16,11 @@ for b in bands:
     fullFilName = filNam + b + '.tif'
     ds = gdal.Open(fullFilName)
     data = np.array(ds.GetRasterBand(1).ReadAsArray())
-    data = data[1274:1309,549:584] #TESTING DATA 
+#    data = data[1283:1293,570:575] #TESTING DATA 
 
     if b == 'BAND21' or b == 'BAND22' or b == 'BAND31' or b == 'BAND32':
-        data = np.int_(np.rint(data))
+#        data = np.int_(np.rint(data))
+        data = data
     if b == 'BAND1' or b == 'BAND2' or b == 'BAND7':
         b = b + 'x1k'
         data = np.int_(np.rint(data*1000))
@@ -37,30 +38,17 @@ dayFlag[np.where(allArrays['SolarZenith'] < 8500)] = 1
 waterFlag = -1
 cloudFlag = -2
 
-#CHANGES LANDMASK TO BINARY (SET DATATYPE) "LAND" = 1
+#CREATE WATER MASK
 waterMask = np.zeros((nRows,nCols),dtype=np.int)
 waterMask[np.where(allArrays['landmask']!=1)] = waterFlag
 
-#CREATE CLOUD MASK (SET DATATYPE) "CLOUD" = 0
-b1plus2gt900=np.zeros((nRows,nCols),dtype=np.int)
-b1plus2gt900[np.where((allArrays['BAND1x1k']+allArrays['BAND2x1k'])>900)] = cloudFlag
+#CREATE CLOUD MASK (SET DATATYPE)
+cloudMask =np.zeros((nRows,nCols),dtype=np.int)
+cloudMask[((allArrays['BAND1x1k']+allArrays['BAND2x1k'])>900)] = cloudFlag
+cloudMask[(allArrays['BAND32']<265)] = cloudFlag
+cloudMask[((allArrays['BAND1x1k']+allArrays['BAND2x1k'])>700)&(allArrays['BAND32']<285)] = cloudFlag
 
-b32lt265 = np.ones((nRows,nCols),dtype=np.int)
-b32lt265[np.where(allArrays['BAND32']<265)] = 0
-
-##MASK CLOUDS AND WATER
-b1plus2gt700=np.zeros((nRows,nCols),dtype=np.int)
-b1plus2gt700[np.where((allArrays['BAND1x1k']+allArrays['BAND2x1k'])>700)] = cloudFlag
-
-b32lt285=np.zeros((nRows,nCols),dtype=np.int)
-b32lt285[np.where(allArrays['BAND32']<285)]=cloudFlag
-
-temp=np.zeros((nRows,nCols),dtype=np.int)
-temp[np.where(b1plus2gt700+b32lt265)==0] = cloudFlag
-
-cloudMask = np.zeros((nRows,nCols),dtype=np.int)
-cloudMask[np.where((b1plus2gt900 == cloudFlag) & (b32lt285 == cloudFlag) & (temp == cloudFlag))] = cloudFlag
-
+#MASK CLOUDS AND WATER FROM INPUT BANDS
 b21CloudWaterMasked = np.copy(allArrays['BAND21'])
 b21CloudWaterMasked[np.where(waterMask == waterFlag)] = waterFlag
 b21CloudWaterMasked[np.where(cloudMask == cloudFlag)] = cloudFlag
@@ -90,20 +78,21 @@ b21saturationVal = 450 #???
 
 bgFlag = -3
 bgMask = np.zeros((nRows,nCols),dtype=np.int)
-bgMask[np.where((dayFlag == 1) & (allArrays['BAND21'] >325) & (deltaT >20))] = bgFlag
-bgMask[np.where((dayFlag == 0) & (deltaT >310)& (deltaT >10))] = bgFlag
+with np.errstate(invalid='ignore'):
+    bgMask[np.where((dayFlag == 1) & (allArrays['BAND21'] >325) & (deltaT >20))] = bgFlag
+    bgMask[np.where((dayFlag == 0) & (deltaT >310)& (deltaT >10))] = bgFlag
 
-b21bgMask = np.copy(b21CloudWaterMasked)
-b21bgMask[np.where(bgMask == bgFlag)] = bgFlag
+    b21bgMask = np.copy(b21CloudWaterMasked)
+    b21bgMask[np.where(bgMask == bgFlag)] = bgFlag
 
-b22bgMask = np.copy(b22CloudWaterMasked)
-b22bgMask[np.where(bgMask == bgFlag)] = bgFlag
+    b22bgMask = np.copy(b22CloudWaterMasked)
+    b22bgMask[np.where(bgMask == bgFlag)] = bgFlag
 
-b31bgMask = np.copy(b31CloudWaterMasked)
-b31bgMask[np.where(bgMask == bgFlag)] = bgFlag
+    b31bgMask = np.copy(b31CloudWaterMasked)
+    b31bgMask[np.where(bgMask == bgFlag)] = bgFlag
 
-deltaTbgMask = np.copy(deltaTCloudWaterMasked)
-deltaTbgMask[np.where(bgMask == bgFlag)] = bgFlag
+    deltaTbgMask = np.copy(deltaTCloudWaterMasked)
+    deltaTbgMask[np.where(bgMask == bgFlag)] = bgFlag
 #############################
 #ALL REQUIRED FUNCTION DEFS
 #############################
@@ -130,8 +119,8 @@ def meanFilt(kernel,kSize,minKsize,maxKsize):
         nghbrCnt = len(nghbrs)
         
         if ((nghbrCnt > minNcount) & (nghbrCnt > (minNfrac * ((kSize **2))))):
-            bgMean = np.int_(np.rint(np.mean(nghbrs)))
-            
+ #           bgMean = np.int_(np.rint(np.mean(nghbrs)))
+             bgMean = np.mean(nghbrs)
     return bgMean
 
 def MADfilt(kernel,kSize,minKsize,maxKsize):
@@ -147,7 +136,8 @@ def MADfilt(kernel,kSize,minKsize,maxKsize):
         nghbrCnt = len(nghbrs)
         
         if ((nghbrCnt > minNcount) & (nghbrCnt > (minNfrac * ((kSize **2))))):
-            bgMean = np.int_(np.rint(np.mean(nghbrs)))
+ #           bgMean = np.int_(np.rint(np.mean(nghbrs)))
+            bgMean = np.mean(nghbrs)
             meanDists = np.abs(nghbrs - bgMean)
             bgMAD = np.mean(meanDists)
             
@@ -241,17 +231,20 @@ if (np.nanmax(b21CloudWaterMasked) > b21saturationVal):
     b22meanFilt = runFilt(b22bgMask,meanFilt,minKsize,maxKsize)
     b22minusBG = np.copy(b22CloudWaterMasked)  - np.copy(b22meanFilt)
 
-    b21minusBG[(b21CloudWaterMasked >= b21saturationVal)] = b22minusBG[(b21CloudWaterMasked >= b21saturationVal)]
+    with np.errstate(invalid='ignore'):
+        b21minusBG[(b21CloudWaterMasked >= b21saturationVal)] = b22minusBG[(b21CloudWaterMasked >= b21saturationVal)]
 
 ####POTENTIAL FIRE TEST
 potFire = np.zeros((nRows,nCols),dtype=np.int)
-potFire[(dayFlag == 1)&(allArrays['BAND21']>310)&(deltaT>10)&(allArrays['BAND2x1k']<300)] = 1
-potFire[(dayFlag == 0)&(allArrays['BAND21']>305)&(deltaT>10)] = 1
+with np.errstate(invalid='ignore'):
+    potFire[(dayFlag == 1)&(allArrays['BAND21']>310)&(deltaT>10)&(allArrays['BAND2x1k']<300)] = 1
+    potFire[(dayFlag == 0)&(allArrays['BAND21']>305)&(deltaT>10)] = 1
 
 # ABSOLUTE THRESHOLD TEST (Kaufman et al. 1998) FOR REMOVING SUNGLINT
 absValTest = np.zeros((nRows,nCols),dtype=np.int)
-absValTest[(dayFlag == 1) & (allArrays['BAND21']>360)] = 1
-absValTest[(dayFlag == 0) & (allArrays['BAND21']>305)] = 1
+with np.errstate(invalid='ignore'):
+    absValTest[(dayFlag == 1) & (allArrays['BAND21']>360)] = 1
+    absValTest[(dayFlag == 0) & (allArrays['BAND21']>305)] = 1
 
 #########################################
 #CONTEXT TESTS (GIGLIO ET AL 2003)
@@ -264,16 +257,19 @@ deltaTmeanFilt = runFilt(deltaTbgMask,meanFilt,minKsize,maxKsize)
 ####deltaT MAD Filtering
 deltaTMADFilt = runFilt(deltaTbgMask,MADfilt,minKsize,maxKsize) 
 deltaTMADfire = np.zeros((nRows,nCols),dtype=np.int)
-deltaTMADfire[deltaT>(deltaTmeanFilt + (3.5*deltaTMADFilt))] = 1
+with np.errstate(invalid='ignore'):
+    deltaTMADfire[deltaT>(deltaTmeanFilt + (3.5*deltaTMADFilt))] = 1
 
 ####CONTEXT FIRE TEST 3
 deltaTfire = np.zeros((nRows,nCols),dtype=np.int)
-deltaTfire[np.where(deltaT > (deltaTmeanFilt + 6))] = 1 
+with np.errstate(invalid='ignore'):
+    deltaTfire[np.where(deltaT > (deltaTmeanFilt + 6))] = 1 
 
 ####CONTEXT FIRE TEST 4
 B21fire = np.zeros((nRows,nCols),dtype=np.int)
-b21MADfilt = runFilt(b21bgMask,MADfilt,minKsize,maxKsize) 
-B21fire[(b21CloudWaterMasked > (b21meanFilt + (3*b21MADfilt)))] = 1
+b21MADfilt = runFilt(b21bgMask,MADfilt,minKsize,maxKsize)
+with np.errstate(invalid='ignore'):
+    B21fire[(b21CloudWaterMasked > (b21meanFilt + (3*b21MADfilt)))] = 1
 
 ####CONTEXT  FIRE TEST 5
 b31meanFilt = runFilt(b31bgMask,meanFilt,minKsize,maxKsize)
@@ -284,12 +280,14 @@ B31fire[(b31CloudWaterMasked > (b31meanFilt + b31MADfilt - 4))] = 1
 
 ###CONTEXT FIRE TEST 6
 rejB21bgFires = np.copy(b21CloudWaterMasked)
-rejB21bgFires[(bgMask != bgFlag)] = bgFlag #PROCESS BG PIXELS
+with np.errstate(invalid='ignore'):
+    rejB21bgFires[(bgMask != bgFlag)] = bgFlag #PROCESS BG PIXELS
 
 b21rejMADfilt = runFilt(rejB21bgFires,MADfilt,minKsize,maxKsize)
 
 B21rejFire = np.zeros((nRows,nCols),dtype=np.int)
-B21rejFire[(b21rejMADfilt>5)] = 1
+with np.errstate(invalid='ignore'):
+    B21rejFire[(b21rejMADfilt>5)] = 1
 
 ############################################
 ####DESERT BOUNDARY TESTS
@@ -300,67 +298,79 @@ B21rejFire[(b21rejMADfilt>5)] = 1
 fireLocTentative = deltaTMADfire*deltaTfire*B21fire
 
 fireLocB31andB21rejFire = np.zeros((nRows,nCols),dtype=np.int)
-fireLocB31andB21rejFire[np.where((B21rejFire == 1)|(B31fire == 1))]= 1
+with np.errstate(invalid='ignore'):
+    fireLocB31andB21rejFire[np.where((B21rejFire == 1)|(B31fire == 1))]= 1
 fireLocTentativeDay = potFire*fireLocTentative*fireLocB31andB21rejFire
 
 dayFires = np.zeros((nRows,nCols),dtype=np.int)
-dayFires[(dayFlag == 1)&((absValTest == 1)|(fireLocTentativeDay ==1))] = 1
+with np.errstate(invalid='ignore'):
+    dayFires[(dayFlag == 1)&((absValTest == 1)|(fireLocTentativeDay ==1))] = 1
 
 #NIGHTTIME DEFINITE FIRES
 nightFires = np.zeros((nRows,nCols),dtype=np.int)
-nightFires[((dayFlag == 0)&((fireLocTentative == 1)|absValTest == 1))] = 1
+with np.errstate(invalid='ignore'):
+    nightFires[((dayFlag == 0)&((fireLocTentative == 1)|absValTest == 1))] = 1
 
 ###########################################
 #####ADDITIONAL DAYTIME TESTS
 ##############################################
 
-#sunglint rejection CHECK ALL UNITS of inputs to thresholds (e.g. thetaG, BAND1x1k)
-
+#sunglint rejection
 relAzimuth = allArrays['SensorAzimuth']-allArrays['SolarAzimuth']
 cosThetaG = (np.cos(allArrays['SensorZenith'])*np.cos(allArrays['SolarZenith']))- (np.sin(allArrays['SensorZenith'])*np.sin(allArrays['SolarZenith'])*np.cos(relAzimuth))
 thetaG = np.arccos(cosThetaG)
 thetaG = (thetaG/3.141592)*180
-#thetaG = ((thetaG+3.141592)/(2*3.141592))*360 #NOT SURE WHICH ONE TO USE
+#thetaG = ((thetaG+3.141592)/(2*3.141592))*360
 
 #SUNGLINT TEST 8
 sgTest8 = np.zeros((nRows,nCols),dtype=np.int)
-sgTest8[np.where(thetaG < 2)] = 1
+with np.errstate(invalid='ignore'):
+    sgTest8[np.where(thetaG < 2)] = 1
 
 #SUNGLINT TEST 9
 sgTest9 = np.zeros((nRows,nCols),dtype=np.int)
-sgTest9[np.where((thetaG<8)&(allArrays['BAND1x1k']>100)&(allArrays['BAND2x1k']>200)&(allArrays['BAND7x1k']>120))] = 1
+with np.errstate(invalid='ignore'):
+    sgTest9[np.where((thetaG<8)&(allArrays['BAND1x1k']>100)&(allArrays['BAND2x1k']>200)&(allArrays['BAND7x1k']>120))] = 1
 
 #SUNGLINT TEST 10
 waterLoc = np.zeros((nRows,nCols),dtype=np.int)
-waterLoc[np.where(waterMask == waterFlag)] = 1
+with np.errstate(invalid='ignore'):
+    waterLoc[np.where(waterMask == waterFlag)] = 1
 nWaterAdj = ndimage.generic_filter(waterLoc, adjWater, size = 3)
 nRejectedWater = runFilt(waterMask,nRejectWaterFilt,minKsize,maxKsize)
-nRejectedWater[np.where(nRejectedWater<0)] = 0
+with np.errstate(invalid='ignore'):
+    nRejectedWater[np.where(nRejectedWater<0)] = 0
                            
 sgTest10 = np.zeros((nRows,nCols),dtype=np.int)
-sgTest10[np.where((thetaG<12) & ((nWaterAdj+nRejectedWater)>0))] = 1
+with np.errstate(invalid='ignore'):
+    sgTest10[np.where((thetaG<12) & ((nWaterAdj+nRejectedWater)>0))] = 1
 
 sgAll = np.zeros((nRows,nCols),dtype=np.int)
-sgAll[(sgTest8 == 1) | (sgTest9 == 1) | (sgTest10 == 1)] = 1
+with np.errstate(invalid='ignore'):
+    sgAll[(sgTest8 == 1) | (sgTest9 == 1) | (sgTest10 == 1)] = 1
 
 #desert boundary rejection
 
 nValid = runFilt(b21bgMask,nValidFilt,minKsize,maxKsize)
 
 nRejectedBG = runFilt(bgMask,nRejectBGfireFilt,minKsize,maxKsize)
-nRejectedBG[np.where(nRejectedBG<0)] = 0
+with np.errstate(invalid='ignore'):
+    nRejectedBG[np.where(nRejectedBG<0)] = 0
 
 #DESERT BOUNDARY TEST 11
 dbTest11 = np.zeros((nRows,nCols),dtype=np.int)
-dbTest11[np.where(nRejectedBG>(0.1*nValid))] = 1
+with np.errstate(invalid='ignore'):
+    dbTest11[np.where(nRejectedBG>(0.1*nValid))] = 1
 
 #DB TEST 12
 dbTest12 = np.zeros((nRows,nCols),dtype=np.int)
-dbTest12[(nRejectedBG>=4)] = 1
+with np.errstate(invalid='ignore'):
+    dbTest12[(nRejectedBG>=4)] = 1
 
 #DB TEST 13
 dbTest13 = np.zeros((nRows,nCols),dtype=np.int)
-dbTest13[np.where(allArrays['BAND2x1k']>150)] = 1
+with np.errstate(invalid='ignore'):
+    dbTest13[np.where(allArrays['BAND2x1k']>150)] = 1
 
 #DB TEST 14
 #ON REJECTED PIXELS MEAN T4
@@ -368,32 +378,39 @@ b21rejBG = np.copy(b21CloudWaterMasked)
 b21rejBG[np.where(bgMask != bgFlag)] = bgFlag #Evalate pixels rejected as BG
 b21rejMeanFilt = runFilt(b21rejBG,meanFilt,minKsize,maxKsize)
 dbTest14 = np.zeros((nRows,nCols),dtype=np.int)
-dbTest14[(b21rejMeanFilt<345)&(b21rejMeanFilt != -4)] = 1
+with np.errstate(invalid='ignore'):
+    dbTest14[(b21rejMeanFilt<345)&(b21rejMeanFilt != -4)] = 1
 
 #DB TEST 15
 dbTest15 = np.zeros((nRows,nCols),dtype=np.int)
-dbTest15[(b21rejMADfilt<3)&(b21rejMeanFilt != -4)] = 1
+with np.errstate(invalid='ignore'):
+    dbTest15[(b21rejMADfilt<3)&(b21rejMeanFilt != -4)] = 1
 
 #DB TEST 16
 dbTest16 = np.zeros((nRows,nCols),dtype=np.int)
-dbTest16[(b21CloudWaterMasked<(b21rejMeanFilt+(6*b21rejMADfilt)))&(b21rejMADfilt != -4)&(b21rejMeanFilt != -4)] = 1
+with np.errstate(invalid='ignore'):
+    dbTest16[(b21CloudWaterMasked<(b21rejMeanFilt+(6*b21rejMADfilt)))&(b21rejMADfilt != -4)&(b21rejMeanFilt != -4)] = 1
 
 dbAll = dbTest11*dbTest12*dbTest13*dbTest14*dbTest15*dbTest16
 #CHUCK OUT ANYTHING THAT FULFILLS ALL DESERT BOUNDARY CRITERIA
 
 #coastal false alarm rejection
-ndvi = (allArrays['BAND2x1k']+allArrays['BAND1x1k'])/(allArrays['BAND2x1k']+allArrays['BAND1x1k'])
+with np.errstate(invalid='ignore'):
+    ndvi = (allArrays['BAND2x1k']+allArrays['BAND1x1k'])/(allArrays['BAND2x1k']+allArrays['BAND1x1k'])
 unmaskedWater = np.zeros((nRows,nCols),dtype=np.int)
 uwFlag = -6
-unmaskedWater[((ndvi<0) & (allArrays['BAND7x1k']<50)&(allArrays['BAND2x1k']<150))] = -6
-unmaskedWater[(bgMask == bgFlag)] = bgFlag
+with np.errstate(invalid='ignore'):
+    unmaskedWater[((ndvi<0) & (allArrays['BAND7x1k']<50)&(allArrays['BAND2x1k']<150))] = -6
+    unmaskedWater[(bgMask == bgFlag)] = bgFlag
 Nuw = runFilt(unmaskedWater,nUnmaskedWaterFilt,minKsize,maxKsize)
 rejUnmaskedWater = np.zeros((nRows,nCols),dtype=np.int)
-rejUnmaskedWater[(absValTest == 0) & (Nuw>0)] = 1
+with np.errstate(invalid='ignore'):
+    rejUnmaskedWater[(absValTest == 0) & (Nuw>0)] = 1
 
 #COMBINE ALL MASKASKAKAKAKSSSSSSS
 allFires = dayFires+nightFires
-allFires[(sgAll == 1) | (dbAll == 1) | (rejUnmaskedWater == 1)] = 0
+with np.errstate(invalid='ignore'):
+    allFires[(sgAll == 1) | (dbAll == 1) | (rejUnmaskedWater == 1)] = 0
 
 #Currently yields 502938 fires, perhaps many outside of study area?
 ##SHOULD YIELD ~176 FIRES FOR 2004178.2120          
@@ -445,12 +462,13 @@ areaKmSq = Pt * Ps
 
 frpMwKmSq = frpMWabs/areaKmSq
 
-inds=np.where((frpMwKmSq>0)&(frpMwKmSq<1000))
+with np.errstate(invalid='ignore'):
+    inds=np.where((frpMwKmSq>0)&(frpMwKmSq<1000))
 FRPlats = allArrays['LAT'][inds]
 FRPlons =allArrays['LON'][inds]
 FrpInds = frpMwKmSq[inds]
 exportCSV = np.column_stack([FRPlons,FRPlats,FrpInds])
-np.savetxt("frpMwKm_2004178_2120_20151115iv.csv", exportCSV, delimiter=",")
+np.savetxt("frpMwKm_2004178_2120_20151117_float.csv", exportCSV, delimiter=",")
 
 
 
