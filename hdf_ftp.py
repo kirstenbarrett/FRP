cc468804447acd55cde714b1c7c974f1cf7675c5
squareorbit <privatecgriff@gmail.com>
2016-07-29 14:49:10 +0100

    FTP now downloads the corresponding 03 02 in sequence

--- hdf_ftp.py
+++ hdf_ftp.py
@@ -4,6 +4,7 @@ import argparse
 import pycurl
 import os.path
 from io import BytesIO
+import datetime
 
 # Argument parser, run with -h for more info
 parser = argparse.ArgumentParser()
@@ -37,30 +38,72 @@ order = output.getvalue().decode('UTF-8').split()
 
 dlCount = 0
 
-# Download all HDFs in the order info
-for info in order:
+# Let's get both the HDF03s and HDF02s
+HDF03 = [hdf for hdf in order if ".hdf" in hdf and "D03" in hdf]
+HDF02 = [hdf for hdf in order if ".hdf" in hdf and "D02" in hdf]
 
-  if ".hdf" not in info:
-    continue
+# Download all HDF02s with the corresponding HDF03s if they exist
+for hdf02 in HDF02:
 
   # The HDF already exists skip the download
-  if os.path.exists(info) and args.verbose:
-    print("Skipping download of " + info)
+  if os.path.exists(hdf02) and args.verbose:
+    print("Skipping download of " + hdf02)
     continue
 
   if args.verbose:
-    print("Attempting download of " + info)
+    print("Attempting download of " + hdf02)
 
-  fp = open(os.path.join('.', info), "wb")
+  fp = open(os.path.join('.', hdf02), "wb")
   curl = pycurl.Curl()
-  curl.setopt(pycurl.URL, host + info)
+  curl.setopt(pycurl.URL, host + hdf02)
   curl.setopt(pycurl.WRITEDATA, fp)
   curl.perform()
   curl.close()
   fp.close()
 
   if args.verbose:
-    print("Successfully downloaded " + info)
+    print("Successfully downloaded " + hdf02)
+    print("Searching for corresponding HDF03 file")
+
+  # Let's get the corresponding HDF03
+  filSplt = hdf02.split('.')
+  datTim = filSplt[1].replace('A', '') + filSplt[2]
+  t = datetime.datetime.strptime(datTim, "%Y%j%H%M")
+
+  julianDay = str(t.timetuple().tm_yday)
+  jZeros = 3 - len(julianDay)
+  julianDay = '0' * jZeros + julianDay
+  yr = str(t.year)
+  hr = str(t.hour)
+  hrZeros = 2 - len(hr)
+  hr = '0' * hrZeros + hr
+  mint = str(t.minute)
+  mintZeros = 2 - len(mint)
+  mint = '0' * mintZeros + mint
+  datNam = yr + julianDay + '.' + hr + mint
+
+  hdf03found = False
+  # Get the corresponding HDF03
+  for filNamCandidate in HDF03:
+    if datNam in filNamCandidate:
+      hdf03 = filNamCandidate
+      hdf03found = True
+      break
+
+  if args.verbose:
+    if hdf03found:
+      print("Corresponding HDF03 file found")
+      print("Attempting download of " + hdf03)
+      fp = open(os.path.join('.', hdf03), "wb")
+      curl = pycurl.Curl()
+      curl.setopt(pycurl.URL, host + hdf03)
+      curl.setopt(pycurl.WRITEDATA, fp)
+      curl.perform()
+      curl.close()
+      fp.close()
+      print("Successfully downloaded " + hdf03)
+    else:
+      print("No corresponding HDF03 file found")
 
   dlCount += 1
 
