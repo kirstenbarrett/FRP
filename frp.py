Cloud masking fix

frp.py
@@ -723,7 +723,7 @@ def process(filMOD02, commandLineArgs, cwd):
     cloudMask2[(allArrays['BAND2x1k'] > 250) & (allArrays['BAND32'] < 300)] = cloudFlag
     cloudMask2[np.where(waterMask == waterFlag)] = cloudFlag
 
-    cloudMask[np.where(cloudMask2, cloudMask == cloudFlag, cloudFlag)] = cloudFlag
+    cloudMask[(cloudMask == cloudFlag) & (cloudMask2 == cloudFlag)] = cloudFlag
 
     # Mask clouds and water from input bands
     b21CloudWaterMasked = np.copy(allArrays['BAND21'])  # ONLY B21